b7ffed58a5e0872234a901f71b8156733dbf7d2d
NidroidX <67051882+NidroidX@users.noreply.github.com>
2024-04-20 17:09:40 +0200

    Update rpc.go

--- app/rpc/rpc.go
+++ app/rpc/rpc.go
@@ -1,11 +1,11 @@
 package rpc
 
 import (
-	"github.com/sedracoin/sedrad/app/appmessage"
-	"github.com/sedracoin/sedrad/app/rpc/rpccontext"
-	"github.com/sedracoin/sedrad/app/rpc/rpchandlers"
-	"github.com/sedracoin/sedrad/infrastructure/network/netadapter"
-	"github.com/sedracoin/sedrad/infrastructure/network/netadapter/router"
+	"github.com/NidroidX/kestrelcoind/app/appmessage"
+	"github.com/NidroidX/kestrelcoind/app/rpc/rpccontext"
+	"github.com/NidroidX/kestrelcoind/app/rpc/rpchandlers"
+	"github.com/NidroidX/kestrelcoind/infrastructure/network/netadapter"
+	"github.com/NidroidX/kestrelcoind/infrastructure/network/netadapter/router"
 	"github.com/pkg/errors"
 )
 
